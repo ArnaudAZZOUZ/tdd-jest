6f34804ca64f6ed239d46f0486df93acbaeb4045
ArnaudAZZOUZ <arnaud.azzouz-thuderoz@le-campus-numerique.fr>
2020-08-26 14:56:27 +0200

    test 2

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,13 @@
 // Implement the sum function here
+
+function sum(a, b) {
+    if (!isNaN(a) && !isNaN(b)) {
+        return a + b;
+    }
+    else return "error number"
+}
+
+
+module.exports = sum;
+
+
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,15 @@
-// Add the unit tests of the sum function here
+const sum = require('../src/sum');
+const multiply = require('../src/multiply');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('add  a letter return error', () => {
+    expect(sum(1, "u")).toBe("error number");
+    expect(sum("n","l")).toMatch
+});
+
+test('2 multiply 3 to get 6', () => {
+    expect(multiply(2,3)).toEqual(6);
+    });
